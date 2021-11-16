25170f588679ce0d23da386b05474f147104c20d
Jozze05 <jose.alvaradoarce@ucr.ac.cr>
2021-11-16 17:02:47 -0600

    Actualizacion de funciones js

--- app.js
+++ app.js
@@ -1,26 +1,58 @@
-let pokemonId = 35; //Variable que almacena el ID del pokemon a obtener aleatoriamente
+let pokemonId = 1; //Variable que almacena el ID del pokemon a obtener aleatoriamente
 let pokemonName = "";
-let api_url = "https://pokeapi.co/api/v2/pokemon/" + pokemonId + "/" //URL de la api que recibe como parametro el ID del pokemon a obtener
+//let api_url = "https://pokeapi.co/api/v2/pokemon/" + pokemonId + "/"; //URL de la api que recibe como parametro el ID del pokemon a obtener
 let imgContainer = document.getElementById("imgContainer");
-let pokemonData = {}
-
+let pokemonData = {};
+getPokemon(pokemonId);
 
 //Funcion para obtener un numero random y asignarlo al ID del pokemon--------------------------------------------------
 
-
-/*Aqui va el codigo para obtener un numero random y agregarlo a la variable pokemonId*/ 
-
-
-//Funcion para obtener pokemon desde la API--------------
-async function getPokemon() {
-    
-
-    await fetch(api_url).then(response => response.json()).then(data => pokemonData = data );
-
-        pokemonName = pokemonData.name
-        imgContainer.src = pokemonData.sprites.front_default
-        console.log(pokemonName)
-
+function getRandom() {
+  return Math.floor(Math.random() * (1, 899)) + 1;
 }
 
-getPokemon();
+//Funcion para obtener pokemon desde la API-----------------------------------------------------
+async function getPokemon(pokemonId) {
+  const call = await fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonId + "/");
+  const pokemonData = await call.json();
+
+  pokemonName = pokemonData.name;
+  imgContainer.src = pokemonData.sprites.front_default;
+  console.log(pokemonName);
+
+  return pokemonData;
+}
+
+
+//Función para obtener el input del usuario -------------------------------------------------------
+function getUserInput(){
+let userInput = document.getElementById("userInput").value;
+return userInput;
+}
+
+//Función al presionar el botón
+async function validatePokemon(){
+
+   let pokemonDataReturned = await getPokemon(pokemonId);
+
+if(pokemonDataReturned.name == getUserInput()){
+
+    console.log("Correcto")
+    getPokemon(getRandom())
+
+}else{
+
+    console.log("Incorrecto, perdiste")
+}
+
+    }
+    
+
+let guessButton = document.getElementById("guess").addEventListener("click", function(){
+
+validatePokemon();
+
+});
+
+
+//Hay que arreglar un bug cuando se pasa al segundo pokemon
